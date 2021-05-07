04f38e1ab1d2d982fad2be73fc87a5881738a679
Casey Calkins <caseycalkins@Caseys-MBP.attlocal.net>
2021-05-07 09:28:10 -0500

    Commit

---
+++ django-project/store/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=true)
+    slug = models.SlugField(max_length=255, unique=true)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
