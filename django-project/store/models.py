2eab5be9ce7db227efe520080d3a430a9b308575
Casey Calkins <caseycalkins@Caseys-MBP.attlocal.net>
2021-05-10 20:10:31 -0500

    Built models and created migration

--- django-project/store/models.py
+++ django-project/store/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255, db_index=true)
-    slug = models.SlugField(max_length=255, unique=true)
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
         verbose_name_plural = 'categories'
@@ -13,3 +15,22 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_created')
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+    
